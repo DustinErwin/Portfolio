39671a0cc18b1ce396966c1511b7d247c093eb17
Galahad <derwin499@gmail.com>
2020-11-13 20:03:38 -0500

    added page structure and styling

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-var item = document.querySelector(".squares");
-
-window.addEventListener("wheel", function (e) {
-  document;
-  if (e.deltaY > 0) item.scrollLeft = item.scrollLeft + 100;
-  else item.scrollLeft -= 100;
-});
